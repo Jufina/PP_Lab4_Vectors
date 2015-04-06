7c2d4b066f87b9dcbb3b3d4ea19d10f57d2ccd08
Julia Samoshchenko <nightrina@gmail.com>
2015-04-06 13:57:57 +0400

    Реализована основа программы(класс Vector с необходимыми функциями). Так же, в классе Main создаются несколько объектов для понимания работы класса.

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Julia on 06.04.2015.
+ */
+public class Main {
+    public static void main(String args[]) {
+        Vector Axy = new Vector(2.0, 1.5, "A");
+        Vector Axyz = new Vector(2.0, 1.5,-3,"B");
+        Vector Some,Some2;
+        Some=Vector.composeVector(2,-7,11,-5,"Some");
+        Some2=Vector.composeVector(2,5,3,4,1,6,"Some2");
+
+        System.out.println(Axy);
+        System.out.println(Axyz);
+        System.out.println(Some);
+        System.out.println(Some2);
+    }
+}
---
+++ src/Vector.java
@@ -0,0 +1,74 @@
+/**
+ * Created by Julia on 06.04.2015.
+ */
+public class Vector {
+    String name;
+    double x;
+    double y;
+    double z;
+
+    public Vector(double x, double y, String name) {
+        this.x = x;
+        this.y = y;
+        this.z = 0;
+        this.name = name;
+    }
+
+    public Vector(double x, double y, double z, String name) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        if(z == 0) {
+            return "Вектор "+name+" {" + x + ", " + y + "}\n";
+        }
+        else {
+            return "Вектор "+name+" {" + x + ", " + y + ", "+z+"}\n";
+        }
+    }
+
+    public static Vector composeVector(double xbeg, double ybeg, double xend, double yend, String name) {
+        return new Vector(xend-xbeg,yend-ybeg, name);
+    }
+
+
+    public static Vector composeVector(double xbeg, double ybeg,double zbeg, double xend, double yend, double zend, String name) {
+        return new Vector(xend-xbeg,yend-ybeg,zend-zbeg, name);
+    }
+}
